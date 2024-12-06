16ee6ccf40fbc205828ad740e4ba4b6abc8410b0
Andrey Bakov <andrey.bakov.2006@gmail.com>
2024-12-07 00:05:48 +0300

    add temp file delete

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
+import logging
 import os
 
 import pyshark
-import logging
-
 from config import config
 from modules.sniffer import Sniffer
-from utils.filter import create_bpf_filter, rename_traffic_dump, convert_to_decode_as
+from utils.filter import convert_to_decode_as, create_bpf_filter, rename_traffic_dump
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -13,7 +12,7 @@ logging.info("Начало захвата HTTP трафика на хосте...
 
 traffic_dump_file_path = config.TRAFFIC_DUMP_FILE_PATH
 database_path = config.DATABASE_PATH
-services= config.SERVICES
+services = config.SERVICES
 
 
 def main():
@@ -23,7 +22,9 @@ def main():
     interface = config.INTERFACE
     bpf_filter = create_bpf_filter(services)
     decode_as = convert_to_decode_as(services)
+
     logging.info(f"Фильтр захвата трафика: {bpf_filter}")
+    logging.info(f"Фильтр захвата трафика: {decode_as}")
 
     rename_traffic_dump(config.TRAFFIC_DUMP_FILE_PATH)
 
--- web/api/traffic_dump.py
+++ web/api/traffic_dump.py
@@ -1,17 +1,30 @@
+import asyncio
 import os
-
-from fastapi import Depends, HTTPException, APIRouter
-from fastapi.responses import FileResponse
+import shutil
+import tempfile
 
 from api import verify_cookie_token
 from config import config
-
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
+from fastapi.responses import FileResponse
 
 router = APIRouter(prefix='')
 
+async def delete_file_after_delay(file_path: str, delay_seconds: int):
+    await asyncio.sleep(delay_seconds)
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
 @router.get("/download-traffic-dump", response_class=FileResponse, dependencies=[Depends(verify_cookie_token)])
-async def download_traffic_dump():
+async def download_traffic_dump(background_tasks: BackgroundTasks):
     if not os.path.exists(config.TRAFFIC_DUMP_FILE_PATH):
         raise HTTPException(status_code=404, detail="File not found")
-    return FileResponse(config.TRAFFIC_DUMP_FILE_PATH, filename="traffic_dump.pcap")
+
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file_name = temp_file.name
+        shutil.copy(config.TRAFFIC_DUMP_FILE_PATH, temp_file_name)
+
+    background_tasks.add_task(delete_file_after_delay, temp_file_name, 600)
+
+    response = FileResponse(temp_file_name, filename="traffic_dump.pcap")
+    return response
